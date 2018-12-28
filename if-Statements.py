32b294b58adcc705c3e0398437ae4a9296d01439
Rumen Stoev <46021188+VidocqRR@users.noreply.github.com>
2018-12-28 12:29:50 +0000

    Create if-Statements.py

---
+++ if-Statements.py
@@ -0,0 +1,12 @@
+
+def Solve(num1,num2,num3):
+
+    if int(num1)==int(num2) and int(num2)==int(num3) and int(num1)==int(num3):
+        return True
+    else:
+         return False
+
+
+
+
+Solve(5,6,6.5)
